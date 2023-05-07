7d32b70d9d9a98aec0fe5025ead99e19e36ea1cf
reytiartur <101744356+reytiartur@users.noreply.github.com>
2023-05-07 18:26:21 +0200

    Authentication. (#2)
    * HOC for dismissing keyboard on click outside.
    
    * Authentication w/Firebase. Token persistance w/SecureStore. Authentication UI.

---
+++ components/SignIn.tsx
@@ -0,0 +1,158 @@
+import {
+  NativeSyntheticEvent,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native"
+import { useState } from "react"
+import AuthInput from "./AuthInput"
+import { LinearGradient } from "expo-linear-gradient"
+import { SafeAreaView } from "react-native-safe-area-context"
+import { AntDesign } from "@expo/vector-icons"
+import { resetPassword, signInUserWithEmail } from "../utils/firebase"
+
+type Props = {
+  styles: any
+  setComponent: React.Dispatch<React.SetStateAction<string>>
+}
+
+const SignIn = ({ styles, setComponent }: Props) => {
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  })
+
+  const handleChange = (name: string, text: string) => {
+    setInputs({ ...inputs, [name]: text })
+  }
+
+  const handleSubmit = async (e: NativeSyntheticEvent<Object>) => {
+    e.preventDefault()
+    await signInUserWithEmail(inputs.email, inputs.password)
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.header}>Sign In to your Account</Text>
+      </View>
+
+      <View style={styles.authContainer}>
+        <View style={styles.inputsContainer}>
+          <AuthInput
+            label="Email"
+            textContentType="emailAddress"
+            value={inputs.email}
+            id="email"
+            onChangeText={(text) => handleChange("email", text)}
+          />
+          <AuthInput
+            label="Password"
+            textContentType="password"
+            secureTextEntry={true}
+            value={inputs.password}
+            id="password"
+            onChangeText={(text) => handleChange("password", text)}
+          />
+        </View>
+
+        <TouchableOpacity onPress={() => resetPassword(inputs.email)}>
+          <Text style={styles.renewPass}>Forgot Password?</Text>
+        </TouchableOpacity>
+
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={(e) => handleSubmit(e)}
+          >
+            <LinearGradient
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              colors={["#f5c889", "#edaf51"]}
+              end={{ x: 0.9, y: 0.9 }}
+            >
+              <Text style={styles.btnText}>Sign In</Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.loginContainer}>
+          <Text style={styles.loginText}>Or login with</Text>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              gap: 12,
+              width: "auto",
+            }}
+          >
+            {Platform.OS === "ios" ? (
+              <TouchableOpacity
+                style={[
+                  styles.button,
+                  styles.providerBtn,
+                  { backgroundColor: "#000" },
+                ]}
+                onPress={() =>
+                  alert(
+                    "Sorry, the Apple SignIn is not available at the moment. Please, create an account with your email and password."
+                  )
+                }
+              >
+                <AntDesign name="apple1" size={24} color="white" />
+                <Text style={styles.btnText}>Sign in with Apple</Text>
+              </TouchableOpacity>
+            ) : null}
+            <TouchableOpacity
+              style={[
+                styles.button,
+                styles.providerBtn,
+                { borderWidth: 1, borderColor: "#000" },
+              ]}
+              onPress={() =>
+                alert(
+                  "Sorry, the Google SignIn is not available at the moment. Please, create an account with your email and password."
+                )
+              }
+            >
+              <AntDesign name="google" size={24} color="#000" />
+              <Text style={[styles.btnText, { color: "#000" }]}>
+                Sign in with Google
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <SafeAreaView
+          style={styles.navigateContainer}
+          edges={["bottom", "left", "right"]}
+        >
+          <Text style={styles.navigateText}>No account yet?</Text>
+          <TouchableOpacity onPress={() => setComponent("SignUp")}>
+            <Text
+              style={[
+                styles.navigateText,
+                {
+                  color: "#edaf51",
+                  paddingHorizontal: 6,
+                  fontWeight: "500",
+                },
+              ]}
+            >
+              Sign Up.
+            </Text>
+          </TouchableOpacity>
+        </SafeAreaView>
+      </View>
+    </View>
+  )
+}
+
+export default SignIn
+
+const styles = StyleSheet.create({})
---
+++ utils/firebase.ts
@@ -0,0 +1,93 @@
+import { initializeApp, FirebaseError } from "firebase/app"
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  User,
+  sendPasswordResetEmail,
+} from "firebase/auth"
+import useAuth from "../hooks/useAuth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAGYX6AGvw1ONgjmT-Q-dvyGWFJd-YHi30",
+  authDomain: "fitness-app-a99fb.firebaseapp.com",
+  projectId: "fitness-app-a99fb",
+  storageBucket: "fitness-app-a99fb.appspot.com",
+  messagingSenderId: "232305164803",
+  appId: "1:232305164803:web:169913cffbbdbf40220436",
+}
+
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+
+const { saveToken, deleteToken } = useAuth()
+
+export const createUserWithEmail = async (
+  email: string,
+  password: string,
+  repeatPassword: string
+) => {
+  if (!email || !password) return
+  if (password !== repeatPassword) alert("Passwords do not match!")
+
+  try {
+    const { user } = await createUserWithEmailAndPassword(auth, email, password)
+    const token = await user.getIdToken(true)
+    saveToken(token)
+  } catch (error) {
+    if (error instanceof FirebaseError) {
+      if (error.code === "auth/email-already-in-use") {
+        alert("Email already in use.")
+      } else {
+        console.log("User creation error", error.message)
+      }
+    } else {
+      alert("Unexpected error")
+    }
+  }
+}
+
+export const signInUserWithEmail = async (email: string, password: string) => {
+  if (!email || !password) return
+  try {
+    const { user } = await signInWithEmailAndPassword(auth, email, password)
+    const token = await user.getIdToken(true)
+    saveToken(token)
+  } catch (error) {
+    if (error instanceof Error) {
+      alert("Sign in error!")
+    } else {
+      alert("Unknown error occurred")
+    }
+  }
+}
+
+export const signOutUser = async () => {
+  await signOut(auth)
+  deleteToken()
+}
+
+export const resetPassword = async (email: string) => {
+  try {
+    await sendPasswordResetEmail(auth, email)
+    alert("We sent you an email. Check your inbox!")
+  } catch (error) {
+    if (error instanceof FirebaseError) {
+      if (error.code === "auth/missing-email") {
+        alert("Provide us with your email first.")
+      } else {
+        alert(error.message)
+      }
+    } else {
+      alert("Unexpected error")
+    }
+  }
+}
+
+export const onAuthStateChangedListener = (
+  setUser: React.Dispatch<React.SetStateAction<User | null>>
+) => {
+  onAuthStateChanged(auth, async (currentUser) => setUser(currentUser))
+}
